b0868ee8dd24090e75d1d978b005c54f26266040
Abhi Gupta <Abhi@Abhis-MacBook.local>
2018-01-17 12:39:17 -0500

    migrated parse core backend to firebase

--- src/sample/Img.java
+++ src/sample/Img.java
@@ -10,15 +10,15 @@ package sample;
 import javafx.scene.image.Image;
 
 public class Img {
-    public final static Image smallClouds = new Image("http://files.parsetfss.com/58db5470-3935-4dcb-9d72-450e368b3899/tfss-be592d5a-3cfd-496c-be25-8c1b96e78d85-SmallClouds.png");
-    public final static Image bigClouds = new Image("http://files.parsetfss.com/58db5470-3935-4dcb-9d72-450e368b3899/tfss-6f343c1d-47c0-44d4-b055-d7b29148a416-BigClouds.png");
-    public final static Image birdRight = new Image("http://files.parsetfss.com/58db5470-3935-4dcb-9d72-450e368b3899/tfss-bccc1fab-68cc-474a-ac13-dc44e145f979-Bird.png");
-    public final static Image birdLeft = new Image("http://files.parsetfss.com/58db5470-3935-4dcb-9d72-450e368b3899/tfss-e5a39a06-7db8-4547-933f-b574b946d53a-Bird%20Left.png");
-    public final static Image lightBox = new Image("http://files.parsetfss.com/58db5470-3935-4dcb-9d72-450e368b3899/tfss-83d82012-c41e-475f-b828-c51c453d8b04-BoxFinal.png");
-    public final static Image darkBox = new Image("http://files.parsetfss.com/58db5470-3935-4dcb-9d72-450e368b3899/tfss-26f2d75a-595a-419b-9356-192b5a25ea8b-BoxDarkFinal.png");
-    public final static Image warningSignOn = new Image("http://files.parsetfss.com/58db5470-3935-4dcb-9d72-450e368b3899/tfss-40443593-ebdb-4e9a-8885-0b59396a205d-WarningSignOn.png");
-    public final static Image warningSignOff = new Image("http://files.parsetfss.com/58db5470-3935-4dcb-9d72-450e368b3899/tfss-cb98dbef-e836-4f7f-ac9a-3e7b6c6c930d-WarningSignOff.png");
-    public final static Image keyRightUp = new Image("http://files.parsetfss.com/58db5470-3935-4dcb-9d72-450e368b3899/tfss-12725bd0-492e-4f00-98c1-0d01e9f413fd-RightKeyUp.png");
-    public final static Image keyRightDown = new Image("http://files.parsetfss.com/58db5470-3935-4dcb-9d72-450e368b3899/tfss-6f9be0f6-7e8a-46c4-a364-71f913176f00-RightKeyDown.png");
-    public final static Image menu = new Image("http://files.parsetfss.com/58db5470-3935-4dcb-9d72-450e368b3899/tfss-2c36c7f8-dd47-434c-8004-7dae87ed9235-Menu.png");
+    public final static Image smallClouds = new Image("https://firebasestorage.googleapis.com/v0/b/run-bird-run.appspot.com/o/SmallClouds.png?alt=media&token=d72a7542-ab9b-4329-8aeb-b862e3db8def");
+    public final static Image bigClouds = new Image("https://firebasestorage.googleapis.com/v0/b/run-bird-run.appspot.com/o/BigClouds.png?alt=media&token=45ab635b-88eb-4213-b735-8fec21cc1b73");
+    public final static Image birdRight = new Image("https://firebasestorage.googleapis.com/v0/b/run-bird-run.appspot.com/o/Bird.png?alt=media&token=85f95713-aba1-4e2f-8fa1-3fb14e6fcd39");
+    public final static Image birdLeft = new Image("https://firebasestorage.googleapis.com/v0/b/run-bird-run.appspot.com/o/Bird%20Left.png?alt=media&token=4d6e2740-856f-4c60-8313-cd8caa509d8d");
+    public final static Image lightBox = new Image("https://firebasestorage.googleapis.com/v0/b/run-bird-run.appspot.com/o/BoxFinal.png?alt=media&token=b92d57bd-3e0d-4a22-ba3e-f09b0047aa80");
+    public final static Image darkBox = new Image("https://firebasestorage.googleapis.com/v0/b/run-bird-run.appspot.com/o/BoxDarkFinal.png?alt=media&token=49979465-d7e2-4ea6-a969-efb152e95157");
+    public final static Image warningSignOn = new Image("https://firebasestorage.googleapis.com/v0/b/run-bird-run.appspot.com/o/WarningSignOn.png?alt=media&token=6ce53f72-eafb-41fd-9f57-f025821b482e");
+    public final static Image warningSignOff = new Image("https://firebasestorage.googleapis.com/v0/b/run-bird-run.appspot.com/o/WarningSignOff.png?alt=media&token=4e318680-c8c5-4d31-82b8-24a62243f409");
+    public final static Image keyRightUp = new Image("https://firebasestorage.googleapis.com/v0/b/run-bird-run.appspot.com/o/RightKeyUp.png?alt=media&token=08133ea4-15bd-47ce-b184-973951a993ce");
+    public final static Image keyRightDown = new Image("https://firebasestorage.googleapis.com/v0/b/run-bird-run.appspot.com/o/RightKeyDown.png?alt=media&token=d4525c4c-febd-487f-a0ac-13149157f180");
+    public final static Image menu = new Image("https://firebasestorage.googleapis.com/v0/b/run-bird-run.appspot.com/o/Menu.png?alt=media&token=f281c256-8c83-4288-b2ba-816daaa9f5aa");
 }
